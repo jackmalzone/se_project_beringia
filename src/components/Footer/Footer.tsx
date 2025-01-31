bb0a384f439c2aa7d99160ab5983155704256830
JAK <56848280+jackmalzone@users.noreply.github.com>
2025-01-30 21:14:17 -0800

    feat: add static seascape divider to footer with solid border

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,12 @@
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../../utils/constants'
 import { clients } from '../../data'
+import { SeascapeDivider } from '../SeascapeDivider/SeascapeDivider'
 import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__divider">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-        </svg>
-      </div>
       <div className="footer__content">
         <div className="footer__column">
           <h3 className="footer__title">About Beringia</h3>
@@ -59,6 +55,7 @@ const Footer = () => {
           © {new Date().getFullYear()} Beringia Marine Technologies. All rights reserved.
         </p>
       </div>
+      <SeascapeDivider height={100} opacity={0.15} />
     </footer>
   )
 }
