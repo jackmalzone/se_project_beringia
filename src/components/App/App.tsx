1111c371c379ac4c32755280fcee84ed3f027f52
JAK <56848280+jackmalzone@users.noreply.github.com>
2025-02-21 00:25:00 -0800

    Fix: Update router and paths for GitHub Pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ModalProvider } from '../../contexts/ModalContext'
 import { ThemeProvider } from '../../contexts/ThemeContext'
 import { LoadingProvider } from '../../contexts/LoadingContext'
